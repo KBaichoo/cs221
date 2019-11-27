336a7c76538a4f5790d75f37a2b59d3fd2720d08
Kevin Baichoo <kbaichoo@stanford.edu>
2019-11-27 14:49:34 -0800

    Fixed mnist model to use 3 classes; output now makes more sense. Trained the net on 10 epochs.

--- model.py
+++ model.py
@@ -20,7 +20,8 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(1, 20, 5, 1)
         self.conv2 = nn.Conv2d(20, 50, 5, 1)
         self.fc1 = nn.Linear(8450, 1000)
-        self.fc2 = nn.Linear(1000, 10)
+        # We simply have 3 classification classes...
+        self.fc2 = nn.Linear(1000, 3)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -31,7 +32,8 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
-    
+
+
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -46,6 +48,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
 
+
 def test(args, model, device, test_loader):
     model.eval()
     test_loss = 0
@@ -54,8 +57,10 @@ def test(args, model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            # sum up batch loss
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            # get the index of the max log-probability
+            pred = output.argmax(dim=1, keepdim=True)
             print(pred)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
@@ -64,6 +69,7 @@ def test(args, model, device, test_loader):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+
 
 def main():
     # Training settings
@@ -84,7 +90,7 @@ def main():
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
-    
+
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     args = parser.parse_args()
@@ -97,22 +103,22 @@ def main():
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
     train_loader = torch.utils.data.DataLoader(
-            datasets.ImageFolder('./frames/train/',
-                       transform=transforms.Compose([
-                           transforms.Grayscale(num_output_channels=1),
-                           transforms.Resize((64,64)),
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
+        datasets.ImageFolder('./frames/train/',
+                             transform=transforms.Compose([
+                                 transforms.Grayscale(num_output_channels=1),
+                                 transforms.Resize((64, 64)),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize((0.1307,), (0.3081,))
+                             ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
-            datasets.ImageFolder('./frames/test/',
-                       transform=transforms.Compose([
-                           transforms.Grayscale(num_output_channels=1),
-                           transforms.Resize((64,64)),
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
+        datasets.ImageFolder('./frames/test/',
+                             transform=transforms.Compose([
+                                 transforms.Grayscale(num_output_channels=1),
+                                 transforms.Resize((64, 64)),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize((0.1307,), (0.3081,))
+                             ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
     '''
@@ -132,14 +138,16 @@ def main():
     '''
 
     model = Net().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr,
+                          momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
         test(args, model, device, test_loader)
 
     if (args.save_model):
-        torch.save(model.state_dict(),"mnist_cnn.pt")
+        torch.save(model.state_dict(), "mnist_cnn.pt")
+
 
 if __name__ == '__main__':
     main()
