775cc2e540fba9f630030f96446195fce4b89e3b
Peter Do <peterhdo@live.com>
2019-12-12 22:23:10 -0800

    plot

--- 7_layer_model.py
+++ 7_layer_model.py
@@ -176,9 +176,9 @@ def main():
                         help='input batch size for training (default: )')
     parser.add_argument('--validation-batch-size', type=int, default=32, metavar='N',
                         help='input batch size for validationing (default: )')
-    parser.add_argument('--epochs', type=int, default=100, metavar='N',
+    parser.add_argument('--epochs', type=int, default=150, metavar='N',
                         help='number of epochs to train (default: 5)')
-    parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
+    parser.add_argument('--lr', type=float, default=0.00001, metavar='LR',
                         help='learning rate (default: 0.01)')
     parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                         help='SGD momentum (default: 0.5)')
