e8bf9f12f7163a009689754ef37e5878c90f3326
Kevin Baichoo <kbaichoo@stanford.edu>
2019-12-12 18:26:08 -0800

    Augmented model.py train to incl accuracy.

--- model.py
+++ model.py
@@ -56,6 +56,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
     """
 
     model.train()
+    correct = 0
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -64,10 +65,20 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
+
+        # To get the accuracy correctly for training epoch
+        # get the index of the max log-probability
+        pred = output.argmax(dim=1, keepdim=True)
+        correct += pred.eq(target.view_as(pred)).sum().item()
+
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
+
+    print('\nTraining set: Accuracy: {}/{} ({:.0f}%)\n'.format(
+        correct, len(train_loader.dataset),
+        100. * correct / len(train_loader.dataset)))
 
 
 def validation(args, model, device, validation_loader):
@@ -124,6 +135,7 @@ class Binarize(object):
     def __call__(self, sample):
         y = torch.zeros(sample.size())
         x = torch.ones(sample.size())
+        # print('mean:{}'.format(torch.mean(sample)))
         return torch.where(sample > self.threshold, x, y)
 
 
